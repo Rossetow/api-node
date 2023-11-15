dfd34f74d7f9cb0e05ffa95405d88bc3d7dc2d37
Rossetow <rafaelrosseto05@gmail.com>
2023-11-14 21:48:39 -0300

    CRUD para os cursos feito

--- minhaAPI/src/server.js
+++ minhaAPI/src/server.js
@@ -23,10 +23,7 @@ server.post('/usuarios', (req, res) =>{
         salvarDados(dados)
         return res.status(201).json({mensagem:"Novo usuário cadastrado com sucesso"})
     }
-
-
 })
-
 
 // Função para atualizar o usuário
 
@@ -57,6 +54,8 @@ server.put('/usuarios/:id', (req, res) =>{
     }
 })
 
+
+
 //Função para deletar usuário
 
 server.delete('/usuarios/:id', (req, res) =>{
@@ -69,6 +68,62 @@ server.delete('/usuarios/:id', (req, res) =>{
     return res.status(200).json({mensagem: 'Usuário deletado com sucesso >:3'})
 })
 
+//Funçã para criar cursos
+
+server.post('/cursos', (req, res) =>{
+    const novoCurso = req.body
+
+    if(!novoCurso.id||!novoCurso.nome_curso||!novoCurso.ch_curso||!novoCurso.professor){
+        return res.status(400).json({mensagem: 'Dados incompletos, tente novamente :/'})
+    } else {
+        dados.cursos.push(novoCurso)
+        salvarDados(dados)
+        return res.status(201).json({mensagem: 'Curso cadastrado com sucesso :D'})
+    }
+})
+
+//Função para editar cursos
+server.put('/cursos/:id', (req, res) =>{
+    // buscar e transformar o id do endpoint em inteiro
+    const cursoId = parseInt(req.params.id)
+
+    //receber o body escrito no postman 
+    const atualizarCurso = req.body
+
+    //encontrar o id no json que ja existe
+    let idCurso = dados.cursos.findIndex(u => u.id === cursoId)
+
+    if(idCurso === -1){
+        return res.status(404).json({mensagem: "Curso não encontrado :/"})
+    } else {
+        //atualiza o nome
+        dados.cursos[idCurso].nome_curso = atualizarCurso.nome_curso || dados.cursos[idCurso].nome_curso
+
+        //atualiza o idade
+        dados.cursos[idCurso].ch_curso = atualizarCurso.ch_curso || dados.cursos[idCurso].ch_curso
+
+        //atualiza o curso
+        dados.cursos[idCurso].professor = atualizarCurso.professor || dados.cursos[idCurso].professor
+
+        salvarDados(dados)
+        res.json({mensagem: "Curso atualizado com sucesso :3"})
+    }
+
+})
+
+
+
+//Função para deletar cursos
+
+server.delete('/cursos/:id', (req, res)=>{
+    const cursoId = parseInt(req.params.id)
+    dados.cursos = dados.cursos.filter(u => u.id != cursoId)
+
+    salvarDados(dados)
+    return res.status(200).json({mensagem: 'Curso deletado com sucesso'})
+})
+
+
 function salvarDados (){
     fs.writeFileSync(__dirname + "/data/dados.json", JSON.stringify(dados, null, 2))
 }
@@ -78,3 +133,9 @@ server.get('/usuarios', (req, res) =>{
     return res.json(dados.users)
 })
 
+
+
+//consumir dados da API == READ do CRUD
+server.get('/cursos', (req, res) =>{
+    return res.json(dados.cursos)
+})
